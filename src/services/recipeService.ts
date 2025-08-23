0c40d1fb2554f897bf6b8780cfe633dce8b49e2a
Olexandr <sashokseven@gmail.com>
2025-08-23 12:38:33 +0300

    create fetch fnc

--- src/services/recipeService.ts
+++ src/services/recipeService.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import type { Recipe } from "../types/recipe";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const recipeApi = axios.create({
-  baseURL: "https://api.spoonacular.com/",
+  baseURL: "https://api.spoonacular.com/recipes/",
   headers: {
     "Content-Type": "application/json",
     accept: "application/json",
@@ -14,3 +15,39 @@ const recipeApi = axios.create({
   },
 });
 
+export interface RandomRecipeHttpResponse {
+  recipes: Recipe[];
+}
+export interface SearchRecipeHttpResponse {
+  results: Recipe[];
+  offset: number;
+  number: number;
+  totalResults: number;
+}
+
+export const fetchRandomRecipes = async (count: number): Promise<Recipe[]> => {
+  const { data } = await recipeApi.get<RandomRecipeHttpResponse>("random", {
+    params: { number: count },
+  });
+  return data.recipes;
+};
+
+export const fetchRecipesByQuery = async (
+  query: string,
+  page: number = 1,
+  pageSize: number = 10
+): Promise<{ recipes: Recipe[]; totalResults: number }> => {
+  const offset = (page - 1) * pageSize;
+  const { data } = await recipeApi.get<SearchRecipeHttpResponse>(
+    "complexSearch",
+    {
+      params: { query, offset, number: pageSize, addRecipeInformation: true },
+    }
+  );
+  return { recipes: data.results, totalResults: data.totalResults };
+};
+
+export const fetchRecipeById = async (id: number): Promise<Recipe> => {
+  const { data } = await recipeApi.get(`${id}/information`);
+  return data;
+};
